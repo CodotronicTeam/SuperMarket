ecf782fd7d52c73679b34ae610b6973f9502b9d1
WINDOWS-HPFO44F\CompuWorld <ahmed_fthe2040@yahoo.com>
2017-11-17 19:19:28 +0200

    i do my update bendary

--- src/UIFrames/Manage_Users.java
+++ src/UIFrames/Manage_Users.java
@@ -5,12 +5,9 @@
  */
 package UIFrames;
 
-import Classes.DBConnection;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
